573009e4c095c000d026d6ebb08c6e0477a87562
Praveen Bisht <prvnbist@gmail.com>
2024-11-24 23:18:02 +0530

    fix(api): fetch flag and appropriate environment

--- src/app/api/flags/[flag]/route.ts
+++ src/app/api/flags/[flag]/route.ts
@@ -1,35 +1,31 @@
 import db from "drizzle";
+import { and, eq } from "drizzle-orm";
 
-export async function GET(
-	request: Request,
-	{ params }: { params: { flag: string } },
-) {
+import { flag, flag_environment } from "drizzle/schema";
+
+export async function GET(request: Request, { params }: { params: { flag: string } }) {
 	const { flag: title } = params;
 
 	const project_id = request.headers.get("Project-Id");
 	const environment = request.headers.get("Environment");
 
 	if (!project_id || !environment) {
-		return new Response(
-			JSON.stringify({ error: "Missing required headers." }),
-			{ status: 400 },
-		);
+		return new Response(JSON.stringify({ error: "Missing required headers." }), { status: 400 });
 	}
 
-	const flag = await db.query.flag.findFirst({
-		columns: { title: true, value: true },
-		where: (f, { and, eq }) =>
-			and(
-				eq(f.title, title),
-				eq(f.project_id, project_id),
-				eq(f.environment, environment),
-			),
-	});
+	const [row] = await db
+		.select({
+			title: flag.title,
+			value: flag_environment.value,
+		})
+		.from(flag)
+		.leftJoin(flag_environment, eq(flag_environment.flag_id, flag.id))
+		.where(and(eq(flag.title, title), eq(flag.project_id, project_id)));
 
-	if (!flag)
+	if (!row)
 		return new Response(JSON.stringify({ error: "No such flag exists." }), {
 			status: 400,
 		});
 
-	return new Response(JSON.stringify(flag), { status: 200 });
+	return new Response(JSON.stringify(row), { status: 200 });
 }
